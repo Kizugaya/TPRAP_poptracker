1addb1f633b73ef332066db546f1863b13b1d4f3
Kizugaya <manuel_poc@yahoo.de>
2025-03-25 18:35:39 +0100

    Initial commit

---
+++ scripts/autotracking.lua
@@ -0,0 +1,3 @@
+ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
+ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
+
---
+++ scripts/init.lua
@@ -0,0 +1,115 @@
+--
+ScriptHost:LoadScript("scripts/control.lua")
+ScriptHost:LoadScript("scripts/autotracking.lua")
+
+
+
+--
+Tracker:AddItems("items/items.json")
+
+
+
+--
+Tracker:AddMaps("maps/maps.json")
+
+
+
+--
+Tracker:AddLayouts("layouts/tracker.json")
+
+
+Tracker:AddLayouts("layouts/layouts_maps.json")
+
+Tracker:AddLayouts("layouts/item_grid.json")
+Tracker:AddLayouts("layouts/bugs.json")
+Tracker:AddLayouts("layouts/keys.json")
+Tracker:AddLayouts("layouts/bosses.json")
+Tracker:AddLayouts("layouts/dungeon_keys.json")
+
+Tracker:AddLayouts("layouts/settings.json")
+
+
+
+--
+Tracker:AddLocations("locations/rooms/overworld.json")
+Tracker:AddLocations("locations/rooms/ft.json")
+Tracker:AddLocations("locations/rooms/gm.json")
+Tracker:AddLocations("locations/rooms/lt.json")
+Tracker:AddLocations("locations/rooms/ag.json")
+Tracker:AddLocations("locations/rooms/sr.json")
+Tracker:AddLocations("locations/rooms/tt.json")
+Tracker:AddLocations("locations/rooms/cs.json")
+Tracker:AddLocations("locations/rooms/pt.json")
+Tracker:AddLocations("locations/rooms/hc.json")
+
+Tracker:AddLocations("locations/overworld/beside_castle_town.json")
+Tracker:AddLocations("locations/overworld/castle_town.json")
+Tracker:AddLocations("locations/overworld/death_mountain.json")
+Tracker:AddLocations("locations/overworld/eldin_field.json")
+Tracker:AddLocations("locations/overworld/faron_field.json")
+Tracker:AddLocations("locations/overworld/faron_woods.json")
+Tracker:AddLocations("locations/overworld/gerudo_desert.json")
+Tracker:AddLocations("locations/overworld/hidden_village.json")
+Tracker:AddLocations("locations/overworld/kakariko.json")
+Tracker:AddLocations("locations/overworld/kakariko_gorge.json")
+Tracker:AddLocations("locations/overworld/lake_hylia.json")
+Tracker:AddLocations("locations/overworld/lanayru_field.json")
+Tracker:AddLocations("locations/overworld/north_eldin.json")
+Tracker:AddLocations("locations/overworld/ordon.json")
+Tracker:AddLocations("locations/overworld/sacred_grove.json")
+Tracker:AddLocations("locations/overworld/snowpeak_mountain.json")
+Tracker:AddLocations("locations/overworld/south_castle_town.json")
+Tracker:AddLocations("locations/overworld/upper_zora's_river.json")
+Tracker:AddLocations("locations/overworld/zora's_domain.json")
+
+Tracker:AddLocations("locations/forest temple/1f.json")
+
+Tracker:AddLocations("locations/goron mines/1f.json")
+Tracker:AddLocations("locations/goron mines/2f.json")
+
+Tracker:AddLocations("locations/lakebed temple/b2.json")
+Tracker:AddLocations("locations/lakebed temple/b1.json")
+Tracker:AddLocations("locations/lakebed temple/1f.json")
+Tracker:AddLocations("locations/lakebed temple/2f.json")
+Tracker:AddLocations("locations/lakebed temple/3f.json")
+Tracker:AddLocations("locations/lakebed temple/4f.json")
+
+Tracker:AddLocations("locations/arbiter's grounds/b2.json")
+Tracker:AddLocations("locations/arbiter's grounds/b1.json")
+Tracker:AddLocations("locations/arbiter's grounds/1f.json")
+Tracker:AddLocations("locations/arbiter's grounds/2f.json")
+Tracker:AddLocations("locations/arbiter's grounds/3f.json")
+Tracker:AddLocations("locations/arbiter's grounds/4f.json")
+
+Tracker:AddLocations("locations/snowpeak ruins/1f.json")
+Tracker:AddLocations("locations/snowpeak ruins/2f.json")
+Tracker:AddLocations("locations/snowpeak ruins/3f.json")
+
+Tracker:AddLocations("locations/temple of time/1f.json")
+Tracker:AddLocations("locations/temple of time/2f.json")
+Tracker:AddLocations("locations/temple of time/3f.json")
+Tracker:AddLocations("locations/temple of time/4f.json")
+Tracker:AddLocations("locations/temple of time/5f.json")
+Tracker:AddLocations("locations/temple of time/6f.json")
+Tracker:AddLocations("locations/temple of time/7f.json")
+Tracker:AddLocations("locations/temple of time/8f.json")
+
+Tracker:AddLocations("locations/city in the sky/b3.json")
+Tracker:AddLocations("locations/city in the sky/b2.json")
+Tracker:AddLocations("locations/city in the sky/b1.json")
+Tracker:AddLocations("locations/city in the sky/1f.json")
+Tracker:AddLocations("locations/city in the sky/2f.json")
+Tracker:AddLocations("locations/city in the sky/3f.json")
+Tracker:AddLocations("locations/city in the sky/4f.json")
+Tracker:AddLocations("locations/city in the sky/5f.json")
+
+Tracker:AddLocations("locations/palace of twilight/1f.json")
+Tracker:AddLocations("locations/palace of twilight/2f.json")
+Tracker:AddLocations("locations/palace of twilight/3f.json")
+Tracker:AddLocations("locations/palace of twilight/4f.json")
+
+Tracker:AddLocations("locations/hyrule castle/1f.json")
+Tracker:AddLocations("locations/hyrule castle/2f.json")
+Tracker:AddLocations("locations/hyrule castle/3f.json")
+Tracker:AddLocations("locations/hyrule castle/4f.json")
+Tracker:AddLocations("locations/hyrule castle/5f.json")
